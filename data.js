873af194769be9a39fab2a88ff89c66d2398cd5e
silvyafr <afriliasilvy@gmail.com>
2026-06-01 13:32:34 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,1 +1,24 @@
+let wisataData =
+JSON.parse(localStorage.getItem("wisataData")) || [
 
+{
+    id:1,
+    nama:"Kebun Raya Bogor",
+    kategori:"Wisata Alam",
+    lokasi:"Bogor",
+    harga:25000,
+    gambar:"https://picsum.photos/500/300?1",
+    deskripsi:"Kebun botani terbesar di Indonesia"
+},
+
+{
+    id:2,
+    nama:"Curug Leuwi Hejo",
+    kategori:"Curug",
+    lokasi:"Sentul",
+    harga:15000,
+    gambar:"https://picsum.photos/500/300?2",
+    deskripsi:"Curug dengan air jernih"
+}
+
+];
